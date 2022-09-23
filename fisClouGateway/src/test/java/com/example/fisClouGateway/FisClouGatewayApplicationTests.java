b347c803c6433b5ecf64fd1c0ed9666aa98227b9
Pratham Padale <113429296+Pratham-Practice@users.noreply.github.com>
2022-09-23 19:18:18 +0530

    Add files via upload

---
+++ fisClouGateway/src/test/java/com/example/fisClouGateway/FisClouGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fisClouGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisClouGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
